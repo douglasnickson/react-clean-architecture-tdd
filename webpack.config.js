aaba2c9372452fbe88c4f866ceef325e4b9521b3
Douglas Nickson <douglas.nickson1@gmail.com>
2022-05-07 13:24:28 -0300

    feat: add login view

--- webpack.config.js
+++ webpack.config.js
@@ -16,29 +16,37 @@ module.exports = {
     }
   },
   module: {
-    rules: [{
-      test: /\.ts(x?)$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.scss$/,
-      use: [{
-        loader: 'style-loader'
-      }, {
-        loader: 'css-loader',
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
         options: {
-          modules: true
+          configFile: path.resolve('./tsconfig.json')
         }
       },
       {
-        loader: 'sass-loader'
-      }]
-    }]
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          options: {
+            modules: true
+          }
+        },
+        {
+          loader: 'sass-loader'
+        }]
+      }
+    ]
   },
   devServer: {
-    contentBase: './public',
-    writeToDisk: true,
-    historyApiFallback: true
+    static: './public',
+    historyApiFallback: true,
+    devMiddleware: {
+      writeToDisk: true
+    }
   },
   externals: {
     react: 'React',
